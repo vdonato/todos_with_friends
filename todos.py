e12669a21b0c62da3625d511723d793f59c52831
Vincent Donato <vincent@streamlit.io>
2021-01-28 15:49:30 -0800

    Let users move tasks from TODO -> done and delete finished ones
    Also simplified the app by removing the placeholders for collaborative
    features for now.

--- todos.py
+++ todos.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -7,28 +9,45 @@ session_state = st.beta_session_state(
     input_key=0,
     my_todos=[],
     my_finished_tasks=[],
-    our_todos=[],
-    our_finished_tasks=[],
 )
+
+
+def render_tasks_and_buttons(tasks, button_label, button_action):
+    for i, t in enumerate(tasks):
+        col.markdown(f"* {t}")
+        col.button(
+            button_label, key=f"{button_label}{i}", on_click=partial(button_action, i)
+        )
+
+
+def move_to_finished(i):
+    move_me = session_state.my_todos.pop(i)
+    session_state.my_finished_tasks.append(move_me)
+
+
+def remove_finished_task(i):
+    session_state.my_finished_tasks.pop(i)
 
 
 def todos(col):
     col.write("### My TODOs")
+    render_tasks_and_buttons(
+        tasks=session_state.my_todos,
+        button_label="Mark Done",
+        button_action=move_to_finished,
+    )
 
 
 def finished_tasks(col):
     col.write("### My Finished Tasks")
+    render_tasks_and_buttons(
+        tasks=session_state.my_finished_tasks,
+        button_label="Remove",
+        button_action=remove_finished_task,
+    )
 
 
-def public_todos(col):
-    col.write("### Public TODOs")
-
-
-def public_finished_tasks(col):
-    col.write("### Public Finished Tasks")
-
-
-render_funcs = [todos, finished_tasks, public_todos, public_finished_tasks]
+render_funcs = [todos, finished_tasks]
 
 
 st.write("# TODOs and Stuff")
@@ -47,7 +66,7 @@ with st.beta_form(submit_label="Submit", key="submit_form"):
         session_state.my_todos.append(todo_text)
         input_placeholder.text_input("Add a TODO!", key=session_state.input_key)
 
-columns = st.beta_columns(4)
+columns = st.beta_columns(2)
 
 for fn, col in zip(render_funcs, columns):
     fn(col)
